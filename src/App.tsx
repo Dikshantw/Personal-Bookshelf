8a0992d4357424aa618af1088014266f05fc2f21
Dikshantw <dikshantw11@gmail.com>
2024-06-11 20:09:35 +0530

    added network request via search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <></>;
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    const books = async () => {
+      const response = await fetch(
+        `https://openlibrary.org/search.json?q=${query}&limit=10&page=1`
+      );
+      const data = await response.json();
+      setResults(data.docs);
+    };
+    books();
+  }, [query]);
+  return (
+    <>
+      <input
+        type="text"
+        name=""
+        id=""
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      <div>
+        {results.map((book) => (
+          <div key={book.title}>
+            <h2>{book.title}</h2>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
